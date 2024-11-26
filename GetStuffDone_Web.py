807e5fa5417f7ba4a0f231efa63d41890e8d6124
mwill20 <105902373+mwill20@users.noreply.github.com>
2024-11-26 05:33:17 -0800

    Add config files for PAAS operablility

--- GetStuffDone_Web.py
+++ GetStuffDone_Web.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -13,6 +15,8 @@ st.title("My GetStuffDone App")
 st.subheader("This is My GetStuffDone App")
 st.write("This app is to MAKE SURE we get stuff done!")
 
+st.text_input(label="", placeholder="What do we need to Get Done..?",
+            on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,5 +26,4 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="", placeholder="What do we need to Get Done..?",
-            on_change=add_todo, key='new_todo')
+
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run GetStuffDone_Web.py
